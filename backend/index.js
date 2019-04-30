ae4087ed001fedc46114d0c3427809a71f153382
pawelWritesCode <p.t.chmielewski@gmail.com>
2019-04-30 14:00:45 +0200

    FEATURE-5 Installed mongoose & connected to db via keys.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
+
 const port = 5000
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongo_uri);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Mongoose connected succesfuly to mongo db');
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
