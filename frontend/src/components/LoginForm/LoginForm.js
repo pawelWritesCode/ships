86c225e4c919e714295f2ad3fcb5b3ce18bede68
Paweł Chmielewski <pawel.chmielewski@edulab.io>
2019-05-13 14:36:50 +0200

    Changed folder structure, started components

---
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <div>
+                <form className="App-LoginForm">
+                    div.form-group
+                </form>
+            </div>
+        )
+    }
+}
+
+export default LoginForm
