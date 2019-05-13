b3d30b4097d84ef39cb8c75ecd801d640e438e83
Paweł Chmielewski <pawel.chmielewski@edulab.io>
2019-05-13 15:44:13 +0200

    SHIPS-8 Created basic login form component, and authentication container

--- frontend/src/components/LoginForm/LoginForm.js
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+import { Form, FormGroup , Label, Input, Button} from 'reactstrap';
 
 class LoginForm extends Component {
     render() {
         return (
-            <div>
-                <form className="App-LoginForm">
+            <Form>
+                <FormGroup>
+                    <Label>Login</Label>
+                    <Input type="text" name="login" className="loginForm-login" placeholder="Type your login" />
+                </FormGroup>
 
-                </form>
-            </div>
+                <FormGroup>
+                    <Label>Password</Label>
+                    <Input type="password" name="password" className="loginForm-password" placeholder="Type your password" />
+                </FormGroup>
+                <Button className="loginForm-button" color="success">Zaloguj</Button>
+            </Form>
         )
     }
 }
--- frontend/src/containers/Authentication/Authentication.js
+++ frontend/src/containers/Authentication/Authentication.js
@@ -1,11 +1,81 @@
 import React, {Component} from 'react';
 import LoginForm from '../../components/LoginForm';
+import {Link} from "react-router-dom";
+import { Button } from 'reactstrap';
+
+const authenticationStates = {
+    login: 'login',
+    register: 'register'
+}
 
 class Authentication extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            type: authenticationStates.login
+        }
+
+        this.changeForm = this.changeForm.bind(this);
+    }
+
+    /**
+     * This method render form according to given type
+     * @param type
+     * @returns {*}
+     */
+    changeForm(e) {
+        const clickedElement = e.target;
+        switch(clickedElement.value) {
+            case authenticationStates.login: {
+                return this.setState({type: authenticationStates.register});
+            }
+            case authenticationStates.register: {
+                return this.setState({type: authenticationStates.login});
+            }
+            default: {
+                return this.setState({type: authenticationStates.login});
+            }
+        }
+    }
+
+    renderForm() {
+        switch(this.state.type) {
+            case authenticationStates.login: {
+                return <LoginForm/>
+            }
+            case authenticationStates.register: {
+                return <div>Register form</div>
+            }
+            default: {
+                return <LoginForm/>
+            }
+        }
+    }
+
     render() {
         return (
-            <div>
-                <LoginForm />
+            <div className="row">
+                <div className="col-md-4"></div>
+                <div className="col-md-4">
+                    <div className="row">
+                        <div className="col-md-12">
+                            {this.renderForm()}
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                        <div className="col-md-12">
+                            <Button color="primary" onClick={this.changeForm} value={this.state.type}> {
+                                this.state.type == authenticationStates.login ?
+                                    "Przejdź do rejestracji" :
+                                    "Przejdź do logowania"
+                            }
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4"></div>
             </div>
         )
     }
