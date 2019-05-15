5c259f40d2f95cbc06aa738998f4d9d93c7aa03f
Paweł Chmielewski <pawel.chmielewski@edulab.io>
2019-05-15 09:51:02 +0200

    SHIPS-9 Added library: "react-notifications", created service with notification generating method

--- frontend/src/containers/App/App.js
+++ frontend/src/containers/App/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { NotificationContainer } from 'react-notifications';
+
+import 'react-notifications/lib/notifications.css';
 
 import { PrivateRoute }  from '../../components/PrivateRoute/PrivateRoute';
 import Dashboard from '../../components/Dashboard';
 import Authentication from "../Authentication";
 
-function App() {
-  return (
-      <div>
-          <Router>
-              <Switch>
-                  <PrivateRoute exact path="/" component={Dashboard} />
-                  <Route exact path="/authentication" component={Authentication} />
-              </Switch>
-          </Router>
-      </div>
-  );
+const App = (props) => {
+
+    return (
+        <div>
+            <NotificationContainer/>
+            <Router>
+                <Switch>
+                    <PrivateRoute exact path="/" component={Dashboard} />
+                    <Route exact path="/authentication" component={Authentication} />
+                </Switch>
+            </Router>
+        </div>
+    );
+
 }
 
 export default App;
