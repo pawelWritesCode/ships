71ae5e75b3a0f5e2f2641e9ca91282b68b356623
Paweł Chmielewski <pawel.chmielewski@edulab.io>
2019-05-16 08:41:57 +0200

    SHIPS-10 Created base for dashboard

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -3,8 +3,23 @@ import React, {Component} from 'react';
 const Dashboard = (props) => {
 
     return (
-        <div>
-            hello dashboard
+        <div className="row">
+            <div className="col-md-2">
+
+            </div>
+            <div className="col-md-8">
+                <div className="row">
+                    <div className="col-md-8">
+                        ActiveRoomList
+                    </div>
+                    <div className="col-md-4">
+                        MainButtonSection
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-2">
+
+            </div>
         </div>
     )
 }
