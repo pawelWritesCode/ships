2f97cfcc8a9a80b75ea56ae79a7f88e6fa3955c1
pawelWritesCode <p.t.chmielewski@gmail.com>
2019-05-16 20:44:58 +0200

    FEATURE-10 Created temp app state, updated ButtonMainSection component

--- frontend/src/components/ButtonMainSection/ButtonMainSection.js
+++ frontend/src/components/ButtonMainSection/ButtonMainSection.js
@@ -1,17 +1,48 @@
 import React, {Component} from 'react';
 import { Button } from 'reactstrap';
+import { appState } from "../../helpers/states";
 
-const ButtonMainSection = (props) => {
+class ButtonMainSection extends Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div className="button-section-container">
-            <div className="button-section">
-                <Button color="primary" size="lg">Profil </Button>
-                <Button color="primary" size="lg">Ranking </Button>
-                <Button color="warning" size="lg">Wyloguj </Button>
+        this.state = {
+            state: appState.menu
+        }
+    }
+
+     buttons(state) {
+        switch (state) {
+            case appState.menu: {
+                return (
+                    <React.Fragment>
+                    <Button color="primary" size="lg">Profil </Button>
+                    <Button color="primary" size="lg">Ranking </Button>
+                    <Button color="warning" size="lg">Wyloguj </Button>
+                    </React.Fragment>
+                )
+            }
+            default: {
+                return (
+                    <React.Fragment>
+                        <Button color="primary" size="lg">Profil </Button>
+                        <Button color="primary" size="lg">Ranking </Button>
+                        <Button color="warning" size="lg">Wyloguj </Button>
+                    </React.Fragment>
+                )
+            }
+        }
+     }
+
+    render() {
+        return (
+            <div className="button-section-container">
+                <div className="button-section">
+                    {this.buttons(this.state)}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default ButtonMainSection
--- frontend/src/helpers/states.js
+++ frontend/src/helpers/states.js
@@ -2,3 +2,9 @@ export const authenticationStates = {
     login: 'login',
     register: 'register'
 }
+
+export const appState = {
+    playing: 'playing',
+    menu: 'menu',
+    options: 'options'
+}
