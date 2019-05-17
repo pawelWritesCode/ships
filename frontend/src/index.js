edf9c2a1286b583b9f5628e0b0b80e96e75a43d5
Paweł Chmielewski <pawel.chmielewski@edulab.io>
2019-05-17 11:23:08 +0200

    SHIPS-12 Applied store to main entry point

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+
+import reducers from './reducers';
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+    ,
+    document.getElementById('root'));
