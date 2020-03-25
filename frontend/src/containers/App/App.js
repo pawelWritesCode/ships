29aed1d9234777cb716c574d69aae1d531a4de9f
Paweł Chmielewski <pawel.chmielewski@edulab.io>
2020-03-25 12:05:00 +0100

    17) Redux - default app state after authentication - idle

--- frontend/src/containers/App/App.js
+++ frontend/src/containers/App/App.js
@@ -13,10 +13,6 @@ import Authentication from "../Authentication";
 class App extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            current: appState.idle
-        }
     }
 
     render() {
